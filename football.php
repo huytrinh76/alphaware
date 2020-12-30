8e29ba63193a9d471f90e685299c84db32896267
Huy Trinh <titaniummurdock@gmail.com>
2020-12-30 21:07:07 +0700

    76

--- football.php
+++ football.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>AlphaWare</title>
+	<title>Sản phẩm</title>
 	<link rel="icon" href="img/logo.jpg" />
 	<link rel = "stylesheet" type = "text/css" href="css/style.css" media="all">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
@@ -25,49 +25,29 @@
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div id="header">
+<div id="header">
 		<img src="img/logo.jpg">
-		<label>alphaware</label>
+		<label>IRON FEVER</label>
 			<ul>
-				<li><a href="#signup"   data-toggle="modal">Sign Up</a></li>
-				<li><a href="#login"   data-toggle="modal">Login</a></li>
+				<li><a href="#signup"   data-toggle="modal">Đăng ký</a></li>
+				<li><a href="#login"   data-toggle="modal">Đăng nhập</a></li>
 			</ul>
 	</div>
 		<div id="login" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="width:400px;">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-				<h3 id="myModalLabel">Login...</h3>
+				<h3 id="myModalLabel">Đăng nhập</h3>
 			</div>
 				<div class="modal-body">
 					<form method="post">
 					<center>
 						<input type="email" name="email" placeholder="Email" style="width:250px;">
-						<input type="password" name="password" placeholder="Password" style="width:250px;">
+						<input type="password" name="password" placeholder="Mật khẩu" style="width:250px;">
 					</center>
 				</div>
 			<div class="modal-footer">
-				<input class="btn btn-primary" type="submit" name="login" value="Login">
-				<button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">Close</button>
-					</form>
-			</div>
-		</div>
-		
-		<div id="login1" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="width:400px;">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-				<h3 id="myModalLabel">Please login before purchasing...</h3>
-			</div>
-				<div class="modal-body">
-					<form method="post">
-					<center>
-						<input type="email" name="email" placeholder="Email" style="width:250px;">
-						<input type="password" name="password" placeholder="Password" style="width:250px;">
-					</center>
-				</div>
-			<div class="modal-footer">
-				<p style="float:left;">No account? <a href="#signup" data-toggle="modal">Sign up here!</a></p>
-				<input class="btn btn-primary" type="submit" name="login" value="Login">
-				<button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">Close</button>
+				<input class="btn btn-primary" type="submit" name="login" value="Đăng nhập">
+				<button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">Đóng</button>
 					</form>
 			</div>
 		</div>
@@ -75,50 +55,50 @@
 		<div id="signup" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="width:700px;">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-					<h3 id="myModalLabel">Sign Up Here...</h3>
+					<h3 id="myModalLabel">Đăng ký</h3>
 				</div>
 					<div class="modal-body">
 						<center>
 					<form method="post">
-						<input type="text" name="firstname" placeholder="Firstname" required>
-						<input type="text" name="mi" placeholder="Middle Initial" maxlength="1" required>
-						<input type="text" name="lastname" placeholder="Lastname" required>
-						<input type="text" name="address" placeholder="Address" style="width:430px;"required>
-						<input type="text" name="country" placeholder="Province" required>
+						<input type="text" name="firstname" placeholder="Họ" required>
+						<input type="text" name="mi" placeholder="Tên đệm" maxlength="1" required>
+						<input type="text" name="lastname" placeholder="Tên" required>
+						<input type="text" name="address" placeholder="Địa chỉ" style="width:430px;"required>
+						<input type="text" name="country" placeholder="Quốc gia" required>
 						<input type="text" name="zipcode" placeholder="ZIP Code" required maxlength="4">
-						<input type="text" name="mobile" placeholder="Mobile Number" maxlength="11">
-						<input type="text" name="telephone" placeholder="Telephone Number" maxlength="8">
+						<input type="text" name="mobile" placeholder="Số điện thoại" maxlength="11">
+						<input type="text" name="telephone" placeholder="Số liên lạc nhà riêng" maxlength="8">
 						<input type="email" name="email" placeholder="Email" required>
-						<input type="password" name="password" placeholder="Password" required>
+						<input type="password" name="password" placeholder="Mật khẩu" required>
 						</center>
 					</div>
 				<div class="modal-footer">
-					<input type="submit" class="btn btn-primary" name="signup" value="Sign Up">
-					<button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">Close</button>
+					<input type="submit" class="btn btn-primary" name="signup" value="Đăng ký">
+					<button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">Đóng</button>
 				</div>
 					</form>
 			</div>
+	
 	<br>
 <div id="container">
-	<div class="nav">
-	
-			 <ul>
-				<li><a href="index.php"><i class="icon-home"></i>Home</a></li>
-				<li><a href="product.php"><i class="icon-th-list"></i>Product</a>
-				<li><a href="aboutus.php"><i class="icon-bookmark"></i>About Us</a></li>
-				<li><a href="contactus.php"><i class="icon-inbox"></i>Contact Us</a></li>
-				<li><a href="privacy.php"><i class="icon-info-sign"></i>Privacy Policy</a></li>
-				<li><a href="faqs.php"><i class="icon-question-sign"></i>FAQs</a></li>
-			</ul>
-	</div>
+<div class="nav">
+	<ul>
+	   <li><a href="index.php"><i class="icon-home"></i>Trang chủ</a></li>
+	   <li><a href="product.php"><i class="icon-th-list"></i>Sản phẩm</a>
+	   <li><a href="aboutus.php"><i class="icon-bookmark"></i>Về chúng tôi</a></li>
+	   <li><a href="contactus.php"><i class="icon-inbox"></i>Hỗ trợ</a></li>
+	   <li><a href="privacy.php"><i class="icon-info-sign"></i>Điều khoản</a></li>
+	   <li><a href="faqs.php"><i class="icon-question-sign"></i>Câu hỏi thường gặp</a></li>
+   </ul>
+</div>
 	
 	<div class="nav1">
 		<ul>
-			<li><a href="product.php">Basketball</a></li>
+			<li><a href="product.php">Giày bóng rổ</a></li>
 			<li>|</li>
-			<li><a href="football.php" class="active" style="color:#111;">Football</a></li>
+			<li><a href="football.php" class="active" style="color:#111;">Giày bóng đá</a></li>
 			<li>|</li>
-			<li><a href="running.php">Running</a></li>
+			<li><a href="running.php">Giày chạy</a></li>
 		</ul>
 	</div>
 	
@@ -148,9 +128,9 @@
 							echo "<a href='details.php?id=".$fetch['product_id']."'><img class='img-polaroid' src='photo/".$fetch['product_image']."' height = '300px' width = '300px'></a>";
 							echo "".$fetch['product_name']."";
 							echo "<br />";
-							echo "P ".$fetch['product_price']."";
+							echo "Giá ".$fetch['product_price']."";
 							echo "<br />";
-							echo "<h3 class='text-info' style='position:absolute; margin-top:-90px; text-indent:15px;'> Size: ".$fetch['product_size']."</h3>";
+							echo "<h3 class='text-info' style='position:absolute; margin-top:-90px; text-indent:15px;'> Kích cỡ: ".$fetch['product_size']."</h3>";
 							echo "</center>";
 							echo "</div>";
 						}
@@ -169,17 +149,15 @@
 	<br />
 	<div id="footer">
 		<div class="foot">
-			<label style="font-size:17px;"> Copyrght &copy; </label>
-			<p style="font-size:25px;">Alphaware Inc. 2015</p>
+			<label style="font-size:17px;"> Copyright &copy; </label>
+			<p style="font-size:25px;">IRON FEVER EST 2017</p>
 		</div>
 			
 			<div id="foot">
 				<h4>Links</h4>
 					<ul>
-						<a href="http://www.facebook.com/alphaware"><li>Facebook</li></a>
-						<a href="http://www.twitter.com/alphaware"><li>Twitter</li></a>
-						<a href="http://www.pinterest.com/alphaware"><li>Pinterest</li></a>
-						<a href="http://www.tumblr.com/alphaware"><li>Tumblr</li></a>
+						<a href="https://www.facebook.com/ironfevervietnam"><li>Facebook</li></a>
+						<a href="https://www.instagram.com/ironfever_vietnam/"><li>Instagram</li></a>
 					</ul>
 			</div>
 	</div>
